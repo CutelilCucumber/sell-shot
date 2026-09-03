a897c637c9854c3c36375fdb3d0c55bfe8f29e6e
Brian <sond.brian@gmail.com>
2026-09-03 14:40:57 -0700

    change api url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,4 +1,4 @@
-const BASE = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const BASE = 'http://sellshot.briansonderen.com/';
 
 function getToken() {
   return localStorage.getItem('token');
